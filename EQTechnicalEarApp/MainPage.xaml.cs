4c81779ab5d948fdc0dd72195ef29929472ee81d
Kimb0 <jakubhromada@gmail.com>
2023-09-08 15:43:55 +0100

    'init'

---
+++ EQTechnicalEarApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EQTechnicalEarApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
